c70e659de9b445ab13b51fa748c68a4bba8b015e
Zahid Hasan Milu <minions.milu889@gmail.com>
2023-03-29 20:27:46 +0600

    login logout authenticate

--- Account/urls.py
+++ Account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .import views
-from Account.views import HomeView,ContactUsView,AboutView
+from Account.views import HomeView,ContactUsView,AboutView,signin
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('login_here/', views.signin, name='login'),
+    # path('login_here/', views.signin, name='login'),
+    path('login/', signin.as_view(), name='login'),
+    path('logount/', views.signout, name='logout'),
     path('register_here/', views.signup, name='register'),
     path('About_Us/', AboutView.as_view(), name='aboutus'),
     path('Contact_Us/', ContactUsView.as_view(), name='contactus'),
--- Account/views.py
+++ Account/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 
@@ -10,33 +10,57 @@ class HomeView(View):
     template_name = 'Account/index.html'
     template_name2 = 'Account/index.html'
 
-    def get(self,request):
-    
+    def get(self, request):
 
-        context={
+        context = {
 
         }
-        return render(request, self.template_name )
+        return render(request, self.template_name)
+
+class signin(View):
+    template_name = 'Account/user-login.html'
+
+
+    def get(self, request):
+        return render(request, self.template_name)
     
-
-
-def signin(request):
-    print('Zahid')
-    if request.method == 'POST':
+    def post(self, request):
         username = request.POST['username']
         password = request.POST['password']
         print(username, password)
-        user = authenticate(username = username, password=password)
+        user = authenticate(username=username, password=password)
         print(user, 'Hello')
         if user:
             login(request, user)
-            print("User logged in succecfully")
+            print("User logged in Successfully")
             return redirect('home')
         else:
             messages.info(request, "User Doesn't Exists")
             return redirect('login')
-    return render(request, 'Account/user-login.html')
 
+# def signin(request):
+#     print('Zahid')
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         print(username, password)
+#         user = authenticate(username=username, password=password)
+#         print(user, 'Hello')
+#         if user:
+#             login(request, user)
+#             print("User logged in succecfully")
+#             return redirect('home')
+#         else:
+#             messages.info(request, "User Doesn't Exists")
+#             return redirect('login')
+#     return render(request, 'Account/user-login.html')
+
+
+
+def signout(request):
+    logout(request)
+    messages.info(request, "Logout Successfully")
+    return redirect('login')
 
 
 def signup(request):
@@ -45,7 +69,7 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
         password1 = request.POST['password1']
-        if password==password1:
+        if password == password1:
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'User Already Exists')
                 return redirect('register')
@@ -53,10 +77,11 @@ def signup(request):
                 messages.info(request, "Email Already Exists")
                 return redirect('register')
             else:
-                obj = User.objects.create_user(username=username, email=email, password=password)
+                obj = User.objects.create_user(
+                    username=username, email=email, password=password)
                 obj.set_password(password)
                 obj.save()
-                messages.success(request, 'User Create Succefully')
+                messages.success(request, 'User Create Successfully')
                 return redirect('login')
         else:
             messages.info(request, "password doesn't match")
@@ -67,11 +92,12 @@ def signup(request):
 class AboutView(View):
     template_name = 'Account/page-about-us.html'
 
-    def get(self,request):
-        return render(request, self.template_name )
+    def get(self, request):
+        return render(request, self.template_name)
+
 
 class ContactUsView(View):
     template_name = 'Account/page-contact.html'
 
-    def get(self,request):
-        return render(request, self.template_name )
+    def get(self, request):
+        return render(request, self.template_name)
