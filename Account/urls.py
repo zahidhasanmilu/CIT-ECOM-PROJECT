3a13e2241a73f54978431dd4d7f41bf4eb917350
Zahid Hasan Milu <minions.milu889@gmail.com>
2023-03-29 17:34:23 +0600

    initial commit

--- Account/urls.py
+++ Account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .import views
-from Account.views import HomeView,LoginView,ContactUsView,AboutView
+from Account.views import HomeView,ContactUsView,AboutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('login_here/', LoginView.as_view(), name='login'),
+    path('login_here/', views.signin, name='login'),
     path('register_here/', views.signup, name='register'),
     path('About_Us/', AboutView.as_view(), name='aboutus'),
     path('Contact_Us/', ContactUsView.as_view(), name='contactus'),
--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -17,18 +20,34 @@ class HomeView(View):
     
 
 
-
-
-class LoginView(View):
-    template_name = 'Account/user-login.html'
-    template_name2 = 'Account/index.html'
-
-    def get(self, request):
-        return render(request, self.template_name)
+def signin(request):
+    if request.
+    return render(request, 'Account/user-login.html')
 
 
 
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        if password==password1:
+            if User.objects.filter(username=name).exists():
+                messages.info(request, 'User Already Exists')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, "Email Already Exists")
+                return redirect('register')
+            else:
+                obj = User.objects.create_user(username=name, email=email, password=password)
+                obj.set_password(password)
+                obj.save()
+                messages.success(request, 'User Create Succefully')
+                return redirect('login')
+        else:
+            messages.info(request, "password doesn't match")
+            return redirect('register')
     return render(request, 'Account/user-register.html')
 
 
